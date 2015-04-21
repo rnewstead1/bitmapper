7a5d84ed1dbe3aa8f7f98c339f3d7fbd6c6abf16
rnewstead1 <racheljnewstead@gmail.com>
2015-04-21 21:53:38 +0100

    Control bitmap by methods rather than input to constructor
    This removes the case statement from the constructor, and means we can
    call the specific method needed.

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,41 +1,62 @@
 class Bitmap
   attr_accessor :bitmap
 
-  def initialize(input_string)
-    input_string.split("\n").each do |line|
-      input = line.split
-      command = input[0]
-      case command
-        when "I"
-          create(input[1].to_i, input[2].to_i)
-        when "L"
-          replace_pixel(input[1].to_i, input[2].to_i, input[3])
-        when "C"
-          clear
-        when "V"
-          vertical_segment(input[1].to_i, input[2].to_i, input[3].to_i, input[4])
-        when "H"
-          horizontal_segment(input[1].to_i, input[2].to_i, input[3].to_i, input[4])
-        when "F"
-          fill(input[1].to_i, input[2].to_i, input[3])
-        when "S"
-          print @bitmap.join("\n")
-        when "X"
-          @bitmap = Array.new
-        else
-          print "[#{ input_string }] is not a valid input"
-      end
-    end
-  end
-
-  private
-
   def create(row_length, number_of_rows)
     @bitmap = Array.new
     (1..number_of_rows).each do |i|
       row = create_row(row_length, "O")
       @bitmap[i-1] = row
     end
+    self
+  end
+
+  def colour(x, y, colour)
+    row = @bitmap[y-1]
+    row[x-1] = colour
+    self
+  end
+
+  def clear
+    @bitmap.map! { |row|
+      create_row(row.length, "O")
+    }
+    self
+  end
+
+  def vertical_segment(column, start_row, end_row, colour)
+    (start_row..end_row).each do |row|
+      colour(column, row, colour)
+    end
+    self
+  end
+
+  def horizontal_segment(row, start_column, end_column, colour)
+    (start_column..end_column).each do |column|
+      colour(column, row, colour)
+    end
+    self
+  end
+
+  def fill(x, y, colour)
+    initial_colour = get_colour(x, y)
+    inner(x, y, colour, initial_colour)
+    self
+  end
+
+  def show
+    print @bitmap.join("\n")
+    self
+  end
+
+  def terminate
+    @bitmap = Array.new
+    self
+  end
+
+  private
+
+  def get_colour(x, y)
+    @bitmap[y-1][x-1]
   end
 
   def create_row(row_length, colour)
@@ -46,48 +67,16 @@ class Bitmap
     row
   end
 
-  def replace_pixel(x, y, colour)
-    row = @bitmap[y-1]
-    row[x-1] = colour
-  end
-
-  def clear
-    @bitmap.map! { |row|
-      create_row(row.length, "O")
-    }
-  end
-
-  def vertical_segment(column, start_row, end_row, colour)
-    (start_row..end_row).each do |row|
-      replace_pixel(column, row, colour)
-    end
-  end
-
-  def horizontal_segment(row, start_column, end_column, colour)
-    (start_column..end_column).each do |column|
-      replace_pixel(column, row, colour)
-    end
-  end
-
-  def fill(x, y, colour)
-    initial_colour = colour(x, y)
-    inner(x, y, colour, initial_colour)
-  end
-
   def inner(x, y, new_colour, initial_colour)
-    replace_pixel(x, y, new_colour)
+    colour(x, y, new_colour)
     surrounding = [[x-1, y], [x+1, y], [x, y-1], [x, y+1]]
     surrounding.each do |x, y|
-      current_pixel_colour = colour(x, y)
+      current_pixel_colour = get_colour(x, y)
       if current_pixel_colour.eql? initial_colour
-        replace_pixel(x, y, new_colour)
+        colour(x, y, new_colour)
         inner(x, y, new_colour, initial_colour)
       end
     end
-  end
-
-  def colour(x, y)
-    @bitmap[y-1][x-1]
   end
 
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -1,50 +1,49 @@
 require "spec_helper"
 
 describe "bitmap" do
-  it "should create a 1 x 1 pixel image with all pixels coloured white for command I" do
-    Bitmap.new("I 1 1").bitmap.should eql ["O"]
+  before(:each) do
+    @bitmap = Bitmap.new
   end
 
-  it "should create a larger image with all pixels coloured white for command I" do
-    Bitmap.new("I 2 3").bitmap.should eql ["OO", "OO", "OO"]
+  it "should create a 1 x 1 pixel image with all pixels coloured white" do
+    @bitmap.create(1, 1).bitmap.should eql ["O"]
   end
 
-  it "should colour pixel (X, Y) with colour C for command L" do
-    Bitmap.new("I 2 2\nL 1 1 A").bitmap.should eql ["AO", "OO"]
+  it "should create a larger image with all pixels coloured white" do
+    @bitmap.create(2, 3).bitmap.should eql ["OO", "OO", "OO"]
   end
 
-  it "should set all pixels to white for command C" do
-    Bitmap.new("I 2 1\nL 1 1 A\nC").bitmap.should eql ["OO"]
+  it "should colour pixel (X, Y) with colour A" do
+    @bitmap.create(2, 2).colour(1, 1, "A").bitmap.should eql ["AO", "OO"]
   end
 
-  it "should create a vertical segment of colour C in coloumn X between rows Y1 and Y2 inclusive for command V" do
-    Bitmap.new("I 3 3\nV 1 2 3 A").bitmap.should eql ["OOO", "AOO", "AOO"]
+  it "should clear all pixels to white" do
+    @bitmap.create(2, 1).colour(1, 1, "A").clear.bitmap.should eql ["OO"]
   end
 
-  it "should create a horizontal segment of colour C in row Y between columns X1 and X2 inclusive" do
-    Bitmap.new("I 3 3\nH 2 2 3 B").bitmap.should eql ["OOO", "OBB", "OOO"]
+  it "should create a vertical segment of colour A in coloumn X between rows Y1 and Y2 inclusive" do
+    @bitmap.create(3, 3).vertical_segment(1, 2, 3, "A").bitmap.should eql ["OOO", "AOO", "AOO"]
+  end
+
+  it "should create a horizontal segment of colour B in row Y between columns X1 and X2 inclusive" do
+    @bitmap.create(3, 3).horizontal_segment(2, 2, 3, "B").bitmap.should eql ["OOO", "OBB", "OOO"]
   end
 
   it "should fill region R with colour C" do
-    Bitmap.new("I 3 4\nF 2 3 C").bitmap.should eql ["CCC", "CCC", "CCC", "CCC"]
+    @bitmap.create(3, 4).fill(2, 3, "C").bitmap.should eql ["CCC", "CCC", "CCC", "CCC"]
   end
 
   it "should only fill surrounding pixels when they are the same colour as X, Y" do
-    Bitmap.new("I 3 4\nH 1 1 2 D\nF 2 3 C").bitmap.should eql ["DDC", "CCC", "CCC", "CCC"]
+    @bitmap.create(3, 4).horizontal_segment(1, 1, 2, "D").fill(2, 3, "C").bitmap.should eql ["DDC", "CCC", "CCC", "CCC"]
   end
 
-  it "should show the contents of the current image for the command S" do
-    out = capture_io{Bitmap.new("I 2 2\nS")}.join ''
+  it "should show the contents of the current image" do
+    out = capture_io { @bitmap.create(2, 2).show }.join ''
     out.should eql "OO\nOO"
   end
 
-  it "should terminate the session for command X" do
-    Bitmap.new("I 3 4\nH 1 1 2 D\nF 2 3 C\nX").bitmap.should eql []
-  end
-
-  it "should not crash with unknown command" do
-    out = capture_io{Bitmap.new("Invalid command")}.join ''
-    out.should eql "[Invalid command] is not a valid input"
+  it "should terminate the session" do
+    @bitmap.create(3, 4).horizontal_segment(1, 1, 2, "D").fill(2, 3, "C").terminate().bitmap.should eql []
   end
 
 end
